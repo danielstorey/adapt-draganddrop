b5636650680b14fce13c3f62098bb86da601bcd0
danielstorey <danielstorey@gmail.com>
2016-12-22 11:27:05 +0000

    Bind disable method to component scope
    Prevents other Drag N Drop components on page from being disabled

--- js/adapt-draganddrop.js
+++ js/adapt-draganddrop.js
@@ -315,7 +315,7 @@ define(function(require) {
 		},
 
 		disableDraggableAnswers: function() {
-			$('.draganddrop-answers').children().draggable('disable');
+			this.$('.draganddrop-answers').children().draggable('disable');
 		},
 
 		markAnswers: function() {
